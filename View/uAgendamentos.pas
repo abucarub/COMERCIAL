b5e030d8ea0aa89b9cae0c4148aeb34747a700a8
Allan C. Abucarub <allan_abu@hotmail.com>
2016-07-20 00:06:30 -0300

    no message

--- View/uAgendamentos.pas
+++ View/uAgendamentos.pas
@@ -10,34 +10,66 @@ uses
   JvExComCtrls, JvMonthCalendar, Vcl.Grids, Vcl.Samples.Calendar,
   JvDateTimePicker, JvExMask, JvToolEdit, Vcl.Mask, RxToolEdit, Data.DB,
   Datasnap.DBClient, JvTimer, JvTimerList, RxSpin, Vcl.Samples.Spin, RxCurrEdit,
-  Vcl.Buttons, Vcl.DBGrids;
+  Vcl.Buttons, Vcl.DBGrids, DBGridCBN;
 
 type
   TfrmAgendamentos = class(TfrmPadrao)
     BuscaPessoa1: TBuscaPessoa;
     BuscaConvenio1: TBuscaConvenio;
     BuscaTabelaPreco1: TBuscaTabelaPreco;
-    pnlHorarios: TPanel;
-    HorarioMarcado1: THorarioMarcado;
-    HorarioMarcado2: THorarioMarcado;
-    HorarioMarcado3: THorarioMarcado;
     cdsHorarios: TClientDataSet;
     JvMonthCalendar1: TJvMonthCalendar;
     rgpHorarios: TGroupBox;
-    DBGrid1: TDBGrid;
     Label2: TLabel;
-    Label4: TLabel;
     btnCriaHorario: TBitBtn;
-    edtCodigo: TCurrencyEdit;
-    Label5: TLabel;
-    Label6: TLabel;
-    CurrencyEdit1: TCurrencyEdit;
     BitBtn1: TBitBtn;
     GroupBox1: TGroupBox;
     speHoras: TSpinEdit;
     speMinutos: TSpinEdit;
     Label3: TLabel;
     Label1: TLabel;
+    StringGrid1: TStringGrid;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Label10: TLabel;
+    Label11: TLabel;
+    Label12: TLabel;
+    Label13: TLabel;
+    Label14: TLabel;
+    Label15: TLabel;
+    Label16: TLabel;
+    Label17: TLabel;
+    Label18: TLabel;
+    Label19: TLabel;
+    Label20: TLabel;
+    Label21: TLabel;
+    Label22: TLabel;
+    Label23: TLabel;
+    Label24: TLabel;
+    Label25: TLabel;
+    Label26: TLabel;
+    Label27: TLabel;
+    Label28: TLabel;
+    Label29: TLabel;
+    Label30: TLabel;
+    Label31: TLabel;
+    Label32: TLabel;
+    Label33: TLabel;
+    Label34: TLabel;
+    Label35: TLabel;
+    Label36: TLabel;
+    Label5: TLabel;
+    edtCodigo: TCurrencyEdit;
+    DBGridCBN1: TDBGridCBN;
+    Label4: TLabel;
+    Label37: TLabel;
+    cdsHorariosDATA_HORA: TDateTimeField;
+    cdsHorariosDIA_SEMANA: TStringField;
+    dsHorarios: TDataSource;
+    Shape1: TShape;
+    Shape2: TShape;
     procedure FormCreate(Sender: TObject);
     procedure BitBtn1Click(Sender: TObject);
   private
@@ -55,7 +87,7 @@ implementation
 
 procedure TfrmAgendamentos.BitBtn1Click(Sender: TObject);
 begin
-  quando gera o numero de parcelas correspondente ao vezes na semana, o botao gerar restantes habilita
+  //quando gera o numero de parcelas correspondente ao vezes na semana, o botao gerar restantes habilita
 end;
 
 procedure TfrmAgendamentos.FormCreate(Sender: TObject);
